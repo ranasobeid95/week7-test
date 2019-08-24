389b3b6246489406e116e197ea839b973995c7e4
rana <ranasobeid95@gmail.com>
2019-08-24 22:17:46 +0300

    Write a handler for the endpoint /cities that gets all the cities data

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const cities = require('./cities');
+
 const router = express.Router();
 
+router.get('/cities',cities.getData);
+router.post('/add-city',cities.postData);
 
 module.exports = router;
